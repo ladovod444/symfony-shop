ab023d6b96115840a4089efd5c272b57affa966b
Dmitrii <ladovod@gmail.com>
2024-10-24 16:56:18 +0500

    Add actions for EasyAdmin, added ability to switch a user

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +22,48 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            EmailField::new('email'),
+            ChoiceField::new('roles')
+                ->setChoices([
+                    'ROLE_ADMIN' => 'ROLE_ADMIN',
+                    'ROLE_USER' => 'ROLE_USER',
+                ])
+                ->allowMultipleChoices()->renderExpanded(),
+            DateTimeField::new('createdAt')->setFormTypeOptions([
+                'years' => range((int)date('Y'), (int)date('Y') + 5),
+                'widget' => 'single_text',
+            ])->setDisabled(), //->hideOnIndex(),
+            DateTimeField::new('updatedAt')->setFormTypeOptions([
+                'years' => range((int)date('Y'), (int)date('Y') + 5),
+                'widget' => 'single_text',
+            ])->setDisabled(),
+            //BooleanField::new('enabled')->onlyOnIndex(),
+            BooleanField::new('enabled'),
+            TextField::new('confirmation_code'),
         ];
     }
-    */
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $impersonate = Action::new('impersonate', 'Impersonate')
+//            ->linkToRoute('admin', function (User $entity) {
+//                return [
+//                    'id' => $entity->getId(),
+//                    '_switch_user' => $entity->getEmail()
+//                    // removed ? before _switch_user
+//                ];
+//            })
+            ->linkToUrl(function (User $entity) {
+                return 'admin/?_switch_user='.$entity->getEmail();
+            })
+        ;
+        return parent::configureActions($actions)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_INDEX, $impersonate);
+    }
+
 }
