cfd915d46ba824e03b3521effd4291d6c9fb7a33
Dmitrii <ladovod@gmail.com>
2024-10-25 21:16:22 +0500

    Add Order adn Order Item factories

---
+++ src/Factory/OrderFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Order;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Order>
+ */
+final class OrderFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Order::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'createdAt' => self::faker()->dateTime(),
+            'owner' => UserFactory::new(),
+            'updatedAt' => self::faker()->dateTime(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Order $order): void {})
+        ;
+    }
+}
