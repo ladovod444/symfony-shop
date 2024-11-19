36e7fcfd5349180dfe4b63c2f478b5bec65d4ae3
dz <ladovod@gmail.com>
2024-11-19 18:39:28 +0500

    Fix Order created event listener

--- src/EventListener/OrderCreated.php
+++ src/EventListener/OrderCreated.php
@@ -1,6 +1,5 @@
 <?php
 
-// src/EventListener/UserChangedNotifier.php
 namespace App\EventListener;
 
 // ...
@@ -22,19 +21,12 @@ use Symfony\Component\Messenger\MessageBusInterface;
 class OrderCreated
 {
     private $entities = [];
+
     public function __construct(
-      private readonly MessageBusInterface $bus) {
+        private readonly MessageBusInterface $bus
+    ) {
 
     }
-    // the entity listener methods receive two arguments:
-    // the entity instance and the lifecycle event
-//    public function postPersist(Order $order, PostUpdateEventArgs $event): void
-//    {
-////        if ($user->isVerified()) {
-////            // Отправим сообщение на email
-////            $this->mailer->notifyUserMessage($user);
-////        }
-//    }
 
     public function postFlush(PostFlushEventArgs $args): void
     {
